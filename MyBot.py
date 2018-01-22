10fe0fb59ead49384191a783a41238a830eae2dd
Ignat228daun <32017583+Ignat228daun@users.noreply.github.com>
2018-01-22 21:49:48 +0300

    Update MyBot.py

--- MyBot.py
+++ MyBot.py
@@ -1,8 +1,7 @@
 import telebot
-token='513665410:AAEUwxNDV6q5Sg_29KwE0n2r9UoPH9Nm30I'
+token='495640848:AAEUjkKbBOXVJlOZGlmdqhFRrQfmSmVfdMw'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def commands(message):
 	bot.send_message(message.chat.id,'Привет даун')
-if __name__=='__main__':
-	bot.polling(none_stop=True)
+
